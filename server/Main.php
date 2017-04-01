7be381b38cd8289b11643e23cd09aaa9ea5179b5
NitzanT <nitzant@users.noreply.github.com>
2017-04-01 19:06:27 +0300

    no message

--- server/Main.php
+++ server/Main.php
@@ -23,24 +23,22 @@ class Main
 //        {
 //            $login->redirect('home.php');
 //        }
-        if(isset($_POST['login']))
-        {
+        if(isset($_POST['login'])) {
             $uname = strip_tags($_POST['username']);
             $umail = strip_tags($_POST['username']);
             $upass = strip_tags($_POST['password']);
-            if(preg_match("/[\W]+/", $uname) || strlen($uname) == 0) {
-                $this->msg = $this->msg . "invalid username!";
+//            if(preg_match("/[\W]+/", $uname) || strlen($uname) == 0) {
+//                $this->msg = $this->msg . "invalid username!";
+//            }
+//            echo $this->msg;
+//            if ($this->msg=="") {
+            if ($login->doLogin($uname, $umail, $upass)) {
+                echo "login successfully!";
+            } else {
+                echo "Wrong Details!";
             }
-            echo $this->msg;
-            if ($this->msg=="") {
-                if ($login->doLogin($uname, $umail, $upass)) {
-                    echo "login successfully!";
-                }
-                else {
-                    echo "Wrong Details!";
-                }
-            }
-            $this->msg = "";
+//            }
+//            $this->msg = "";
         }
         if(isset($_POST['register'])) {
             $uname = trim($_POST['user_name']);
